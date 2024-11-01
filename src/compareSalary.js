0b4b8ac14c94286aab2ad59806d530f14cb3ee3d
Yurca-New <nazaruk.yurii@student.ehu.lt>
2024-11-01 23:36:19 +0200

    qq

--- src/compareSalary.js
+++ src/compareSalary.js
@@ -6,5 +6,22 @@
  * @returns {number}
  */
 module.exports.compareSalary = function compareSalary(firstSalary, secondSalary, thirdSalary) {
+  let maxSalary = 0;
+  let minSalary = 0;
+  if (firstSalary > secondSalary && firstSalary > thirdSalary) {
+    maxSalary = firstSalary;
+  } else if (secondSalary > firstSalary && secondSalary > thirdSalary){
+    maxSalary = secondSalary;
+  } else if (thirdSalary > firstSalary && thirdSalary > secondSalary){
+    maxSalary = thirdSalary;
+  }
+  if (firstSalary < secondSalary && firstSalary < thirdSalary) {
+    minSalary = firstSalary;
+  } else if (secondSalary < firstSalary && secondSalary < thirdSalary){
+    minSalary = secondSalary;
+  } else if (thirdSalary < firstSalary && thirdSalary < secondSalary){
+    minSalary = thirdSalary;
+  }
+  return maxSalary - minSalary;
   throw new Error('Not implemented');
 };
